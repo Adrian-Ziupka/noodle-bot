cafeae92a1386e759869861cf3d0f28ec0077dfb
Adrian Ziupka <adrian.ziupka@student.hpi.uni-potsdam.de>
2019-09-05 21:09:12 +0200

    Finish basic functionality

---
+++ noodle_bot.py
@@ -0,0 +1,60 @@
+from telegram_bot import TelegramBot
+from urllib import request, parse
+from datetime import date
+import json
+
+dishes_url = "https://www.studentenwerk-potsdam.de//essen/unsere-mensen-cafeterien/detailinfos" \
+             "/?tx_typoscriptrendering%5Bcontext%5D=%7B%22record%22%3A%22pages_66%22%2C%22path%22%3A%22tt_content" \
+             ".list.20.ddfmensa_ddfmensajson%22%7D&tx_ddfmensa_ddfmensajson%5Bmensa%5D=4&cHash" \
+             "=b14a1168346e0b7db67da056660d3a0e "
+noodles_available = False
+update_id = None
+bot = TelegramBot("config.cfg")
+
+
+def make_reply(msg):
+    reply = None
+    if msg == "/noodle":
+        if date.today().weekday() < 5:
+            today = date.today().strftime("%d.%m.%Y")
+            reply = "Angebote+der+Mensa+f%C3%BCr+den+" + str(today) + ":%0A%0A"
+            all_dishes = get_todays_dishes()
+            # print(all_dishes)
+            for offer in all_dishes:
+                reply += get_todays_offer(offer) + "%0A"
+            if not noodles_available:
+                reply += "Keine+Nudeln...+Wieso?!"
+        else:
+            reply = "Wer geht am Wochenende schon zur Mensa?"
+    return reply
+
+
+def get_todays_offer(offer):  # noodles_available = True if such a title
+    title = offer["titel"]
+    dish = offer["beschreibung"]
+    price = offer["preis_s"]
+    return parse.quote(f"{title}: {dish} (€{price})")
+
+
+def get_todays_dishes():
+    r = request.urlopen(dishes_url).read().decode("utf-8")
+    content = json.loads(r)
+    # print(content["wochentage"][0]["datum"])
+    return content["wochentage"][0]["datum"]["angebote"]
+
+
+while True:
+    noodles_available = False
+    updates = bot.get_updates(offset=update_id)
+    updates = updates["result"]
+    if updates:
+        for item in updates:
+            update_id = item["update_id"]
+            try:
+                message = item["message"]["text"]
+            except KeyError:
+                message = None
+            from_ = item["message"]["from"]["id"]
+            reply_ = make_reply(message)
+            bot.send_message(from_, reply_)
+
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,18 +1,12 @@
-import configparser
 from urllib import request
+import configparser
 import json
-
-# import requests
-
-r = request.urlopen(
-    "https://www.studentenwerk-potsdam.de/essen/unsere-mensen-cafeterien/detailinfos/?tx_ddfmensa_ddfmensa%5Bmensa%5D=6&tx_ddfmensa_ddfmensa%5Baction%5D=show&cHash=42918cc76cdcb7a31c8fb2187b846933").read()
-print(r.decode("utf-8"))
 
 
 def read_token_from_config_file(config):
     parser = configparser.ConfigParser()
     parser.read(config)
-    return parser.get('creds', 'token') # NOT COMMIT TO GIT (safety issues)
+    return parser.get('creds', 'token')
 
 
 class TelegramBot:
@@ -23,15 +17,15 @@ class TelegramBot:
         self.timeout = 100
 
     def get_updates(self, offset=None):
-        url = self.base + f"/getUpdates?timeout={self.timeout}"
+        url = self.base + f"getUpdates?timeout={self.timeout}"
         if offset:
             url = url + f"&offset={offset + 1}"
-        # r = requests.get(url)
-        # r = urllib.openurl(url)
-        # return json.loads(r)
+        print(url)
+        r = request.urlopen(url).read().decode("utf-8")
+        return json.loads(r)
 
-    def send_message(self, msg, chat_id):
+    def send_message(self, chat_id, msg):
         url = self.base + f"sendMessage?chat_id={chat_id}&text={msg}"
-        # if msg is not None:
-        # requests.get(url)
-        pass
+        if msg is not None:
+            request.urlopen(url)
+
